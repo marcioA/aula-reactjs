794dd13c005fc4d4b480f505fcb2d1988aedaaab
marcioA <zamar_pinheiro@hotmail.com>
2023-03-15 20:55:27 -0300

    primeiro commit, estrutura reactjs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function somaContador() {
+    setCounter(counter + 1);
+  }
+
+  useEffect(() => {
+    console.error(counter);
+  }, [counter]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={somaContador}>Aumentar contador</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {counter}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
